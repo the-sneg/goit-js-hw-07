8c562b085457e7ab09f6a7ccbece48466a4686ae
the-sneg <the.sneg.sasha@gmail.com>
2022-06-02 21:22:04 +0300

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,61 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const refs = {
+  galleryList: document.querySelector(".gallery"),
+};
+
+const galleryListCreat = galleryItems
+  .map((item) => {
+    const renderList = `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+            <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.description}"
+            />
+        </a>
+    </div>`;
+    return renderList;
+  })
+  .join("");
+
+refs.galleryList.innerHTML = galleryListCreat;
+
+// ==========================================================
+
+refs.galleryList.addEventListener("click", onGalleryImgClick);
+
+function onGalleryImgClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const selectedImgUrl = e.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${selectedImgUrl}">
+    `);
+
+  instance.show();
+
+  onEscapeClose(instance);
+}
+
+function onEscapeClose(instance) {
+  const visible = basicLightbox.visible();
+
+  if (visible) {
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "Escape") {
+        instance.close();
+      }
+    });
+  }
+}
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+// ===========================================================
+
+const refs = {
+  galleryList: document.querySelector(".gallery"),
+};
+
+const galleryListCreat = galleryItems
+  .map((item) => {
+    const renderList = `
+    <a class="gallery__item" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>`;
+    return renderList;
+  })
+  .join("");
+
+refs.galleryList.innerHTML = galleryListCreat;
+
+// ================================================================
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+refs.galleryList.addEventListener("click", onGalleryImgClick);
+
+function onGalleryImgClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+}
 
 console.log(galleryItems);
